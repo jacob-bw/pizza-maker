ed2578ff643120061f0b5cfdd3be60e9a3b6ca3f
Jacob <wittenberg.jacob@gmail.com>
2019-09-24 19:44:44 -0500

    finished linking everything

---
+++ pizza-maker/javascripts/components/cheese.js
@@ -0,0 +1,8 @@
+import utilities from '../helpers/utilities.js';
+
+const printCheeseOptions = () => {
+    const domString = 'cheese';
+    utilities.printToDom("Cheeeeeese", 'cheese-counter');
+};
+
+export default { printCheeseOptions }
--- pizza-maker/javascripts/main.js
+++ pizza-maker/javascripts/main.js
@@ -1,1 +1,9 @@
 console.log('gimme pizzaaaaaa');
+
+import cheese from './components/cheese.js';
+
+const init = () => {
+    cheese.printCheeseOptions();
+};
+
+init();
